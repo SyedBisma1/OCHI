2adf396f1a8e2e1c969a01bd11375f2139683a77
Syed Bimsa <Syeda.Bisma.Fatima.1@gmail.com>
2024-08-08 22:28:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './componants/Navbar'
+import LandingPage from './componants/LandingPage'
+
+function App() {
+  return (
+    <div className='h-screen w-full  text-white'>
+     <Navbar/>
+     <LandingPage/>
+    </div>
+  )
+}
+
+export default App
